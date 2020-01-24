fa097736956814184743d4553e4c911974488d3b
Gonzalo <gonzalo.rosendo@aero.tur.ar>
2020-01-24 20:08:41 -0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :movies, only: [:index]
+      resources :seasons, only: [:index]
+      resources :movies_and_seasons, only: [:index]
+      resources :users do
+        resources :pucharse, only: [:create]
+      end
+    end
+  end
+end
