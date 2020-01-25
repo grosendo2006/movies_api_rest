4ccb04a2b376628cef02bc8e0fd59cca0ba49207
Gonzalo <gonzalo.rosendo@aero.tur.ar>
2020-01-25 10:51:42 -0300

    add model to create purchase

---
+++ app/models/api/v1/purchase/create.rb
@@ -0,0 +1,39 @@
+module Api
+  module V1
+    module Purchase
+      class Create
+        def initialize(purchase_option, user)
+          @purchase_option = purchase_option
+          @user            = user
+          @status          = 404
+          @message         = "Couldn't find user or purchase option"
+        end
+
+        def call
+          if @purchase_option && @user
+            if (@user.in_library?(@purchase_option))
+              @status  = 403
+              @message = 'the user has already bought this option'
+            else
+              #@user.purchases << Purchase.create(purchase_option: @purchase_option.content,
+              #                                   user: @user)
+              @user.purchases.create!(purchase_option: @purchase_option)
+              @status  = :created
+              @message = @user.errors.any? ? @user.errors.message : 'Success'
+            end
+          end
+          response
+        end
+
+        private
+
+        def response
+          {
+            message: @message,
+            status:  @status
+          }
+        end
+      end
+    end
+  end
+end
